9a89256d66e810281780037a6a4be4728314dfba
NerokArtworks <nerokprodscontact@gmail.com>
2022-03-30 10:01:19 +0200

    Modifico la clase principal

--- miguel1/src/miguel1/HolaMundo.java
+++ miguel1/src/miguel1/HolaMundo.java
@@ -5,6 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo java-git");
 		System.out.println("Añado segunda línea");
+		System.out.println("Rama cambio1");
 	}
 
 }
